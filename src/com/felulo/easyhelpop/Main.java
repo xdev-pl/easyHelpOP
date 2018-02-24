08d599e183123cc7fd27dbf6cc4befa170d0014c
Paweł Dębiński <Luxq1@wp.pl>
2018-02-24 20:33:20 +0100

    Main.java update

--- src/com/felulo/easyhelpop/Main.java
+++ src/com/felulo/easyhelpop/Main.java
@@ -8,13 +8,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
 	
-	public String translateColors(String msg) {
-		return msg.replace("&", "§");
-	}
-	
 	@Override
 	public void onEnable() {
 		this.getServer().getPluginManager().registerEvents(this, this);
+		
 		this.getLogger().info("easyHelpOP 1.0.1 has been enabled.");
 	}
 	
@@ -23,10 +20,12 @@ public class Main extends JavaPlugin implements Listener {
 		if(cmd.getName().equalsIgnoreCase("helpop")) {
 			if(sender.hasPermission("easyhelpop.use") || sender.hasPermission("easyhelpop.admin") || sender.isOp()) {
 				if(args.length >= 1) {
-					String str = String.join(" ", args);
+					String message = String.join(" ", args);
+					
 					sender.sendMessage(this.translateColors("&a[&7easyHelpOP&a] &2Zgłoszenie zostało wysłane."));
 					sender.sendMessage(this.translateColors("&a[&7easyHelpOP&a] &2Administracja się z tobą skontaktuje jak tylko będzie to możliwe."));
-					Bukkit.broadcast(this.translateColors("&a[&7easyHelpOP&a] &6" + sender.getName().toString() + " &e>>&2 " + str), "easyhelpop.admin");
+					
+					Bukkit.broadcast(this.translateColors("&a[&7easyHelpOP&a] &6" + sender.getName() + " &e>>&2 " + message), "easyhelpop.admin");
 				} else {
 					sender.sendMessage(this.translateColors("&a[&7easyHelpOP&a] &cMusisz podać treść zgłoszenia!"));
 				}
@@ -36,5 +35,8 @@ public class Main extends JavaPlugin implements Listener {
 		}
 		return false;
 	}
-
+	
+	private String color(String s){
+		return s != null ? ChatColor.translateAlternateColorCodes('&', s) : null;
+	}
 }
